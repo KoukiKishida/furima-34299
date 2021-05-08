4e2dc8080cd8535478d4772b1af697c899ebbc4a
KoukiKishida <kk.so.m.84@icloud.com>
2021-05-08 15:31:26 +0900

    新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :encrypted_password, with: PASSWORD_REGEX 
+    validates :name_sei, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :name_mei, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :name_sei_kana, format: { with: /\A[ぁ-んァ-ヶー]+\z/ }
+    validates :name_mei_kana, format: { with: /\A[ぁ-んァ-ヶー]+\z/ }
+    validates :birthday
+  end
+   has_many :comments
+   has_many :products
+   has_many :purchase_records
+  end
 end
