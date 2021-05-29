f0ea8bf27f9e31cc685446496833a95fc6dfefe0
KoukiKishida <kk.so.m.84@icloud.com>
2021-05-29 17:50:20 +0900

    null: false

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_05_15_064950) do
     t.integer "prefectures_id", null: false
     t.integer "day_id", null: false
     t.integer "price", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_products_on_user_id"
