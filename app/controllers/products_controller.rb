05e6235a642c265629d3303f21ecec575a6557a6
KoukiKishida <kk.so.m.84@icloud.com>
2021-06-05 15:24:54 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, except: [:show, :index]
+
 
   def index
     @products = Product.all.order(id: "DESC")
@@ -13,6 +15,20 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    redirect_to root_path unless current_user.id == @product.user_id
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -22,11 +38,14 @@ class ProductsController < ApplicationController
     end
   end
 
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:image, :product_name, :description, :category_id, :prefecture_id, :status_id, :burden_id,
-                                    :day_id, :price).merge(user_id: current_user.id)
+    params.require(:product).permit(:image, :product_name, :description, :category_id,
+       :prefecture_id, :status_id, :burden_id, :day_id, :price).merge(user_id: current_user.id)
   end
-
 end
