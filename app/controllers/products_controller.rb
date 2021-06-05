be2a1c667c07d3d1362fd16f8080a72ddef9a5e8
KoukiKishida <kk.so.m.84@icloud.com>
2021-06-05 16:52:52 +0900

    差分、privateメソッドまとめ

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :move_to_index, except: [:show, :index]
+  before_action :find_product, only: [:edit, :update, :show]
+  before_action :not_user, only: [:edit, :update]
 
 
   def index
@@ -12,16 +13,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
-    redirect_to root_path unless current_user.id == @product.user_id
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path
     else
@@ -38,14 +35,18 @@ class ProductsController < ApplicationController
     end
   end
 
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
-  end
-
   private
 
   def product_params
     params.require(:product).permit(:image, :product_name, :description, :category_id,
        :prefecture_id, :status_id, :burden_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def not_user
+    redirect_to root_path unless current_user.id == @product.user_id
   end
 end
